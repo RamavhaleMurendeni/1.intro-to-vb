35da53e0802bc000c6b9925b36972de9c25ec681
Ramavhale Murendeni <Ramavhale Murendeni@DESKTOP-CO1SVRB>
2024-01-18 21:48:45 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label6 = New Label()
+        ComboBox1 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(268, 304)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(182, 46)
+        Button1.TabIndex = 0
+        Button1.Text = "Welcome Button"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(59, 73)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(59, 108)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Surname"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(59, 141)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(26, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "age"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(59, 175)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(57, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "profesion"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(59, 205)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(44, 15)
+        Label6.TabIndex = 6
+        Label6.Text = "gender"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Teacher", "Lecture", "Police", "Security", "Nurse", "Doctor"})
+        ComboBox1.Location = New Point(165, 173)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 7
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(165, 212)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(51, 19)
+        RadioButton1.TabIndex = 8
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Male"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(165, 237)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(63, 19)
+        RadioButton2.TabIndex = 8
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Female"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(165, 262)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(53, 19)
+        RadioButton3.TabIndex = 8
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "other"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(165, 70)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(121, 23)
+        TextBox1.TabIndex = 9
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(165, 100)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(121, 23)
+        TextBox2.TabIndex = 9
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(165, 133)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(121, 23)
+        TextBox3.TabIndex = 9
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(475, 304)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(182, 46)
+        Button2.TabIndex = 0
+        Button2.Text = "Close"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label6)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
